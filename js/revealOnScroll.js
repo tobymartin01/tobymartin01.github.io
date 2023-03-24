5fbd04fc3f30e90fe06be7678b2685d569cbb87c
ItsJustMarto <tobymartin999@hotmail.com>
2023-03-24 12:55:14 +0000

    update 24.03.23

--- js/revealOnScroll.js
+++ js/revealOnScroll.js
@@ -1,44 +1,29 @@
-    //reveal on scroll left
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach((entry) => {
-            console.log(entry)
-            if (entry.isIntersecting) {
-                entry.target.classList.add('show-scroll');
-            } else {
-                entry.target.classList.remove('show-scroll');
-            }
-        });
+//reveal on scroll left
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show-scroll');
+        } else {
+            entry.target.classList.remove('show-scroll');
+        }
     });
+});
 
-    const hiddenElements = document.querySelectorAll('.hidden-scroll');
-    hiddenElements.forEach((el) => observer.observe(el));
+const hiddenElements = document.querySelectorAll('.hidden-scroll');
+hiddenElements.forEach((el) => observer.observe(el));
 
-    //reveal on scroll right
-    const observer_right = new IntersectionObserver((entries) => {
-        entries.forEach((entry) => {
-            console.log(entry)
-            if (entry.isIntersecting) {
-                entry.target.classList.add('show-scroll-right');
-            } else {
-                entry.target.classList.remove('show-scroll-right');
-            }
-        });
+//reveal on scroll fade
+const observer_fade = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show-scroll-fade');
+        } else {
+            entry.target.classList.remove('show-scroll-fade');
+        }
     });
+});
 
-    const hiddenElementsright = document.querySelectorAll('.hidden-scroll-right');
-    hiddenElementsright.forEach((el) => observer_right.observe(el));
-
-        //reveal on scroll fade
-        const observer_fade = new IntersectionObserver((entries) => {
-            entries.forEach((entry) => {
-                console.log(entry)
-                if (entry.isIntersecting) {
-                    entry.target.classList.add('show-scroll-fade');
-                } else {
-                    entry.target.classList.remove('show-scroll-fade');
-                }
-            });
-        });
-    
-        const hiddenElementsfade = document.querySelectorAll('.hidden-scroll-fade');
-        hiddenElementsfade.forEach((el) => observer_fade.observe(el));
+const hiddenElementsfade = document.querySelectorAll('.hidden-scroll-fade');
+hiddenElementsfade.forEach((el) => observer_fade.observe(el));
